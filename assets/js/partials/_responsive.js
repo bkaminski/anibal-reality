997adbbdd3cdec294fe266b9912c14b26f2ab7fd
Ben Kaminski <ben@benkaminski.com>
2021-06-11 18:01:39 -0400

    changes pulled down from prod

--- assets/js/partials/_responsive.js
+++ assets/js/partials/_responsive.js
@@ -7,9 +7,15 @@
 
 		if ($window.width() < 768) {
 			$('#rick').width(150);
+			$('#jeff').width(150);
+			$('#andrea').width(150);
+			$('.team').addClass('text-center');
 		};
 		if ($window.width() >= 768) {
 			$('#rick').width(768);
+			$('#jeff').width(768);
+			$('#andrea').width(200);
+			$('.team').removeClass('text-center');
 		}
 	}
 	
--- parts/home-testimonials.php
+++ parts/home-testimonials.php
@@ -2,12 +2,12 @@
 	<div class="testimonials">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-4 p-5" style="background-color: #e9e9e9;">
-					<h2 class="display-4 pt-1 customers--saying">What Our Customers Are Saying!</h2>
+				<div class="col-md-4 p-5 pt-4" style="background-color: #e9e9e9;">
+					<h2 class="display-4 pt-1 customers--saying">What Our Customers Are Saying</h2>
 				</div>
-				<div class="col-md-8 pt-5 customer-testimonials">
+				<div class="col-md-8 pt-5 pb-5 customer-testimonials">
 					<div class="testimonial--plugin">
-						<?php echo do_shortcode('[testimonials_slider rotate_per_page=true show_controls=true]'); ?>
+						<?php echo do_shortcode('[testimonials_slider rotate_per_page=true limit=7]'); ?>
 					</div>
 				</div>
 			</div>
